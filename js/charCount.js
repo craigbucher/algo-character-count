06f7af80f06d7096bf2c8eb3b3a482b27c85a0cd
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-26 12:56:57 -0500

    original js

--- js/charCount.js
+++ js/charCount.js
@@ -1,3 +1,22 @@
 exports.charCount = function() {
-
+    function charCount(string){	
+        let result = {};	
+        string = string.toLowerCase();	
+        for (let i = 0; i < string.length; i++){	
+          if (string[i] === ' '){	
+            continue;	
+          }	
+          if (result[string[i]] === undefined){	
+            result[string[i]] = 1;	
+          } else {	
+            result[string[i]] = result[string[i]] + 1;	
+          }	
+        }	
+        let objArr = Object.entries(temp).sort((a,b) => b[1] - a[1]);	
+        let result = {}	
+        for (let j = 0; j < objArr.length; j++){	
+          result[objArr[j][0]] = objArr[j][1];	
+        }	
+        return result;	
+      }
 };
