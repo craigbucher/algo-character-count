d1579bb4a25488a08ab60a2d7377bbdcbdf17def
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-26 16:25:53 -0500

    sort errors

--- js/charCount.js
+++ js/charCount.js
@@ -16,8 +16,9 @@ exports.charCount = function(string) {
         let val = strObj[key];
         result.push([key, val]);
     }
-    // result.sort((a,b) => b[1] - a[1]);
-    result.sort();
+    // need to determine the proper sort function to get seconday alphabetical sort **********
+    result.sort((a,b) => b[1] - a[1]);
+    // result.sort(([a, b], [c, d]) => d - b || c - a);
     return result;	
 };
 console.log(exports.charCount("an apple a day will keep the doctor away"))
